55dc4d1b5e244c77c6a7dc0eeff4e3e34422e01b
Robert Pliszka <30964657+romusek@users.noreply.github.com>
2017-11-11 16:59:06 +0100

    Create ZapisDoPliku.java

---
+++ ZapisDoPliku.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.io.PrintWriter;
+
+public class ZapisDoPliku {
+    public ZapisDoPliku(String path, String content){
+        File file = new File(path);
+        try{
+            PrintWriter pw = new PrintWriter(file);
+            pw.write(content);
+        }catch (Exception e){
+            System.out.println("wyjebalo sie");
+        }
+    }
+}
