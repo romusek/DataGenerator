5b92fcb17539b773d20a9cfd6fc653d6cb959ab9
Robert Pliszka <30964657+romusek@users.noreply.github.com>
2017-11-11 17:40:12 +0100

    Create Constants.java

---
+++ Constants.java
@@ -0,0 +1,6 @@
+public class Constants {
+    final static Integer liczbaArtykulow = 5000;
+    final static Integer liczbaWydan = 250;
+    final static Integer liczbaRedaktorow = 1;
+    final static Integer liczbaStatusArtykulu = 7;
+}
