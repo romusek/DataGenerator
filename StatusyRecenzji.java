ef44e6eead9ac3251691130301f3bffb747b9720
Robert Pliszka <30964657+romusek@users.noreply.github.com>
2017-11-12 14:06:50 +0100

    Create StatusyRecenzji.java

---
+++ StatusyRecenzji.java
@@ -0,0 +1,19 @@
+public class StatusyRecenzji {
+    public static void main(String[]s){
+        Integer licznik = 0;
+        String content = new String();
+        String statusyRecenzji[] = new String[Constants.liczbaStatusRecenzji];
+        statusyRecenzji[0] = "inne1";
+        statusyRecenzji[1] = "inne2";
+        statusyRecenzji[2] = "inne3";
+        statusyRecenzji[3] = "inne4";
+        statusyRecenzji[4] = "inne5";
+        statusyRecenzji[5] = "inne6";
+        statusyRecenzji[6] = "inne7";
+        for (int i=0; i<Constants.liczbaStatusRecenzji; i++){
+            licznik++;
+            content = content + licznik.toString() + ';' + statusyRecenzji[i] + ';' + '\n';
+        }
+        new ZapisDoPliku("StatusyRecenzji.txt", content);
+    }
+}
