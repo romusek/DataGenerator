a022b5663af1d7facf541e2cd06c55be56d50882
Robert Pliszka <30964657+romusek@users.noreply.github.com>
2017-11-12 13:08:57 +0100

    Update Constants.java

--- Constants.java
+++ Constants.java
@@ -3,4 +3,15 @@ public class Constants {
     final static Integer liczbaWydan = 250;
     final static Integer liczbaRedaktorow = 1;
     final static Integer liczbaStatusArtykulu = 7;
+    final static Integer liczbaAutorow = 300;
+    final static Integer liczbaCzytelnikow = 10000;
+    final static Integer liczbaGosci = 123;
+    final static Integer liczbaWlascicieli = 1;
+    final static Integer liczbaTypuPrawa = 4;
+    final static Integer liczbaPrawCzytelnikow = 10000;
+    final static Integer liczbaKategorii = 52;
+    final static Integer liczbaPrzynaleznosciArtykulow = 15000;
+    final static Integer liczbaRecenzetow = 300;
+    final static Integer liczbaRecenzji = 700;
+    final static Integer skalaOcen = 10;
 }
