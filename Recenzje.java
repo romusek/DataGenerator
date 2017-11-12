6d664e7bc50d38a956075cb33737a0b9dfc083e4
Robert Pliszka <30964657+romusek@users.noreply.github.com>
2017-11-12 13:11:42 +0100

    Create Recenzje.java

---
+++ Recenzje.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+
+public class Recenzje {
+    public static void main(String[] sdf){
+        String content = new String();
+        Integer licznik = 0;
+        for (int i=0; i<Constants.liczbaRecenzji; i++){
+            licznik++;
+            content = content + licznik.toString() + ';' + getOcenaArtykulu().toString() + ';' + getIdArtykulu().toString()
+                    + getIdStatusuRecenzji().toString() + ';' + getIdRecenzenta().toString() + ';' + getIdRedaktora() +
+                    ';' + '\n';
+        }
+        new ZapisDoPliku("Recenzje.txt", content);
+    }
+    
+    static Integer getOcenaArtykulu(){
+        return new Random().nextInt(Constants.skalaOcen);
+    }
+    static Integer getIdArtykulu(){
+        return new Random().nextInt(Constants.liczbaArtykulow) + 1;
+    }
+    static  Integer getIdStatusuRecenzji(){
+        return new Random().nextInt(Constants.liczbaStatusArtykulu) + 1;
+    }
+    static Integer getIdRecenzenta(){
+        return new Random().nextInt(Constants.liczbaRecenzetow) + 1;
+    }
+    static Integer getIdRedaktora(){
+        return new Random().nextInt(Constants.liczbaRedaktorow) + 1;
+    }
+}
