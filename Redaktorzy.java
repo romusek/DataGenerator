ae807732a1eb0e7943f0ff2ed90f4e7b0cd42be5
Robert Pliszka <30964657+romusek@users.noreply.github.com>
2017-11-12 17:23:07 +0100

    Update Redaktorzy.java

--- Redaktorzy.java
+++ Redaktorzy.java
@@ -1,13 +1,9 @@
-import java.io.File;
 import java.util.ArrayList;
 import java.util.Random;
-import java.util.Scanner;
 
 public class Redaktorzy {
     static ArrayList<String> imie = new ArrayList<String>();
     static ArrayList<String> nazwisko = new ArrayList<String>();
-    static String tmp1, tmp2;
-    static Scanner scanner;
 
     static Integer licznik = 0;
     static String content = new String();
@@ -17,42 +13,14 @@ public class Redaktorzy {
         for (int i=0; i<Constants.liczbaCzytelnikow; i++){
             idCzytelnika.add(i + 1);
         }
-
-        try{
-            scanner = new Scanner(new File("imiona.txt"));
-            while(scanner.hasNext()){
-                imie.add(scanner.next());
-            }
-            scanner = new Scanner(new File("nazwiska.txt"));
-            while(scanner.hasNext())
-                nazwisko.add(scanner.next());
-        }catch (Exception e){
-            System.out.println("nie ma pliku");
-        }
+        
         for (int i=0; i<Constants.liczbaRedaktorow; i++){
             licznik++;
-            tmp1 = getImie();
-            tmp2 = getNazwisko();
-            content = content + licznik.toString() + ';' +tmp1 + ';' + tmp2 + ';' + genEmail(tmp1, tmp2) + ';' +
-                    getIdCzytelnika() + ';' + '\n';
+            content = content + licznik.toString()  + ';' + getIdCzytelnika() + ';' + '\n';
         }
         new ZapisDoPliku("Redaktorzy.txt", content);
     }
-
-    static String getImie(){
-        Random r = new Random();
-        //imie.size();
-        int liczba = r.nextInt(imie.size());
-        return imie.get(liczba);
-    }
-    static String getNazwisko(){
-        Random r = new Random();
-        int liczba = r.nextInt(nazwisko.size());
-        return nazwisko.get(liczba);
-    }
-    static String genEmail(String im,String naz){
-        return im + '.' + naz + "@wp.com";
-    }
+    
     static Integer getIdCzytelnika(){
         return idCzytelnika.remove(new Random().nextInt(idCzytelnika.size()));
     }
